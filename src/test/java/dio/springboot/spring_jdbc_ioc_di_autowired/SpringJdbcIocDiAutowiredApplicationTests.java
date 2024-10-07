095848e2ce72563829d335ed8610c8d1a9d41679
caiobasilio <caiobasilio@Caios-MacBook-Pro.local>
2024-10-07 15:06:08 -0300

    Commit inicial com Bean e Component.

---
+++ src/test/java/dio/springboot/spring_jdbc_ioc_di_autowired/SpringJdbcIocDiAutowiredApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springboot.spring_jdbc_ioc_di_autowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcIocDiAutowiredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
